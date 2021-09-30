6d7ab3c3714a49f40c6c1481f633a5700af51ce5
katicabogar87 <katicabogar87@gmail.com>
2021-09-30 16:16:12 +0200

    ADDED database connection

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,5 +1,10 @@
+import modul.DBEngine;
+
 public class Application {
     public static void main(String[] args) {
         System.out.println("run");
+        DBEngine dbEngine = new DBEngine();
+        System.out.println(dbEngine.isConnected());
+
     }
 }
