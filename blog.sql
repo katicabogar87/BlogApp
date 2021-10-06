40305e70f0c104b98bca7783e7aa9776250c2ae1
katicabogar87 <katicabogar87@gmail.com>
2021-10-06 16:55:48 +0200

    ADDED sql script updated

--- blog.sql
+++ blog.sql
@@ -9,11 +9,11 @@ login_name VARCHAR(50) UNIQUE NOT NULL,
 password VARCHAR(200),
 email VARCHAR(50) UNIQUE NOT NULL,
 name VARCHAR(50),
-
-role ENUM('admin', 'moderator', 'user')
+role ENUM('admin', 'moderator', 'user'),
+reg_date TIMESTAMP
 );
 
-DROP TABLE IF EXISTS template;
+DROP TABLE IF EXISTS template; 	-- az összes tulajdonságot tartalmazhatná egyetlen css-file
 CREATE TABLE template(
 id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL UNIQUE,
 name VARCHAR(50) UNIQUE NOT NULL,
@@ -29,19 +29,19 @@ id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL UNIQUE,
 name VARCHAR(50) UNIQUE NOT NULL,
 owner_id INT UNSIGNED NOT NULL,
 template_id INT UNSIGNED NOT NULL,
+creation_time TIMESTAMP,
 FOREIGN KEY(owner_id) REFERENCES user(id),
 FOREIGN KEY(template_id) REFERENCES template(id)
 );
-
-
-
 
 
 DROP TABLE IF EXISTS blog_post;
 CREATE TABLE blog_post(
 id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL UNIQUE,
 title VARCHAR(50) UNIQUE NOT NULL,
-post_text VARCHAR(500),
+post_text LONGTEXT,	-- lehetne elérési út is egy fájlnévhez: txt vagy html formátum
+status ENUM ('draft', 'posted', 'invisible'),
+pub_time TIMESTAMP,
 blog_id INT UNSIGNED NOT NULL, 
 FOREIGN KEY(blog_id) REFERENCES blog(id)
 );
@@ -51,7 +51,8 @@ CREATE TABLE comment(
 id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL UNIQUE,
 blog_post_id INT UNSIGNED NOT NULL, 
 author_id INT UNSIGNED NOT NULL, 
-preceding_id INT UNSIGNED NOT NULL,
+preceding_id INT UNSIGNED,
+pub_time TIMESTAMP,
 FOREIGN KEY(blog_post_id) REFERENCES blog_post(id),
 FOREIGN KEY(author_id) REFERENCES user(id),
 FOREIGN KEY(preceding_id) REFERENCES comment(id)
